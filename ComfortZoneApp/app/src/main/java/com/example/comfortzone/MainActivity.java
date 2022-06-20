19998bf15c00dcbaf6008636a919ba76b7b6063f
fionamei <fionamei131@gmail.com>
2022-06-20 14:01:22 -0700

    can logout

--- ComfortZoneApp/app/src/main/java/com/example/comfortzone/MainActivity.java
+++ ComfortZoneApp/app/src/main/java/com/example/comfortzone/MainActivity.java
@@ -1,8 +1,15 @@
 package com.example.comfortzone;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.parse.ParseUser;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,34 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        //inflate the menu
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.logout) {
+            onLogoutButton();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    void onLogoutButton() {
+        ParseUser.logOutInBackground();
+        ParseUser currentUser = ParseUser.getCurrentUser();
+        Toast.makeText(this, "Logged out!", Toast.LENGTH_SHORT).show();
+        goLoginActivity();
+
+    }
+
+    void goLoginActivity() {
+        Intent i = new Intent(this, LoginActivity.class);
+        startActivity(i);
+        finish();
     }
 }
