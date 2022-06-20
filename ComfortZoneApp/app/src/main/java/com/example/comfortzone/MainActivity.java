85cc68b8addd9c3ff602ba1a432b31c78c97d8c4
fionamei <fionamei131@gmail.com>
2022-06-20 15:50:25 -0700

    template for bottom nav

--- ComfortZoneApp/app/src/main/java/com/example/comfortzone/MainActivity.java
+++ ComfortZoneApp/app/src/main/java/com/example/comfortzone/MainActivity.java
@@ -1,14 +1,57 @@
 package com.example.comfortzone;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView bottomNavigationView;
+    final FragmentManager fragmentManager = getSupportFragmentManager();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initViews();
+        listenerSetup();
     }
+
+    private void initViews() {
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+    }
+
+    private void listenerSetup() {
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment fragment;
+                switch (item.getItemId()) {
+                    case R.id.action_home:
+//                        fragment = new FeedFragment();
+                        break;
+                    case R.id.action_input:
+//                        fragment = new ComposeFragment();
+                        break;
+                    case R.id.action_profile:
+                    default:
+//                        fragment = new ProfileFragment();
+                        break;
+                }
+//                fragmentManager.beginTransaction().replace(R.id.flContainer, fragment).commit();
+                return true;
+            }
+        });
+        // setting default selection
+        bottomNavigationView.setSelectedItemId(R.id.action_home);
+    }
+
 }
