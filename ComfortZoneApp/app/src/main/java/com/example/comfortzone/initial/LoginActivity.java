38b2fdf9517e8ad712752763400640a8f005fcb7
fionamei <fionamei131@gmail.com>
2022-07-22 13:51:12 -0700

    [fb login] moved logic out

--- ComfortZoneApp/app/src/main/java/com/example/comfortzone/initial/LoginActivity.java
+++ ComfortZoneApp/app/src/main/java/com/example/comfortzone/initial/LoginActivity.java
@@ -2,7 +2,6 @@ package com.example.comfortzone.initial;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -13,16 +12,13 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.comfortzone.R;
+import com.example.comfortzone.initial.data.FacebookLoginUtil;
 import com.example.comfortzone.ui.HostActivity;
-import com.facebook.AccessToken;
-import com.facebook.GraphRequest;
 import com.parse.LogInCallback;
 import com.parse.ParseException;
 import com.parse.ParseUser;
 import com.parse.SaveCallback;
 import com.parse.facebook.ParseFacebookUtils;
-
-import org.json.JSONException;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -126,7 +122,12 @@ public class LoginActivity extends AppCompatActivity {
                         if (user == null) {
                         } else if (user.isNew()) {
                             Toast.makeText(LoginActivity.this, "Logging in, please wait", Toast.LENGTH_SHORT).show();
-                            getUserDetailFromFB();
+                            FacebookLoginUtil.getUserDetailFromFB(new SaveCallback() {
+                                @Override
+                                public void done(ParseException e) {
+                                    goInitialSetup();
+                                }
+                            });
                         } else {
                             goHostActivity();
                         }
@@ -140,31 +141,6 @@ public class LoginActivity extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         ParseFacebookUtils.onActivityResult(requestCode, resultCode, data);
         super.onActivityResult(requestCode, resultCode, data);
-    }
-
-    private void getUserDetailFromFB() {
-        GraphRequest request = GraphRequest.newMeRequest(AccessToken.getCurrentAccessToken(), (object, response) -> {
-            ParseUser user = ParseUser.getCurrentUser();
-            try {
-                if (object.has(KEY_NAME))
-                    user.setUsername(object.getString(KEY_NAME));
-                if (object.has(KEY_EMAIL))
-                    user.setEmail(object.getString(KEY_EMAIL));
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }
-            user.saveInBackground(new SaveCallback() {
-                @Override
-                public void done(ParseException e) {
-                    goInitialSetup();
-                }
-            });
-        });
-
-        Bundle parameters = new Bundle();
-        parameters.putString(KEY_FIELDS, String.format("%s,%s", KEY_NAME, KEY_EMAIL));
-        request.setParameters(parameters);
-        request.executeAsync();
     }
 
     private void goInitialSetup() {
---
+++ ComfortZoneApp/app/src/main/java/com/example/comfortzone/initial/data/FacebookLoginUtil.java
@@ -0,0 +1,38 @@
+package com.example.comfortzone.initial.data;
+
+import static com.example.comfortzone.initial.LoginActivity.KEY_EMAIL;
+import static com.example.comfortzone.initial.LoginActivity.KEY_FIELDS;
+import static com.example.comfortzone.initial.LoginActivity.KEY_NAME;
+
+import android.os.Bundle;
+
+import com.facebook.AccessToken;
+import com.facebook.GraphRequest;
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.SaveCallback;
+
+import org.json.JSONException;
+
+public class FacebookLoginUtil {
+
+    public static void getUserDetailFromFB(SaveCallback callback) {
+        GraphRequest request = GraphRequest.newMeRequest(AccessToken.getCurrentAccessToken(), (object, response) -> {
+            ParseUser user = ParseUser.getCurrentUser();
+            try {
+                if (object.has(KEY_NAME))
+                    user.setUsername(object.getString(KEY_NAME));
+                if (object.has(KEY_EMAIL))
+                    user.setEmail(object.getString(KEY_EMAIL));
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            user.saveInBackground(callback);
+        });
+
+        Bundle parameters = new Bundle();
+        parameters.putString(KEY_FIELDS, String.format("%s,%s", KEY_NAME, KEY_EMAIL));
+        request.setParameters(parameters);
+        request.executeAsync();
+    }
+}
